a6637382cdda40ecd04b17759821641a7ca231c7
pratistha-j03 <pratisthajais1903@gmail.com>
2025-07-28 16:56:43 +0530

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "FinTrack",
     });
     console.log("MongoDB connected");
   } catch (error) {
