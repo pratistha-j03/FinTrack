82a3645b3415bc96ca6d0535c1632696ac11df45
pratistha-j03 <pratisthajais1903@gmail.com>
2025-07-28 15:20:40 +0530

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error(`Error: ${error.message}`);
